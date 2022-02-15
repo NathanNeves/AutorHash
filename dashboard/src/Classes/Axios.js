eb9e43b31ca39b98ea213ca531ba8d7cfba91172
jotave1999 <80502199+jotave1999@users.noreply.github.com>
2022-02-14 23:13:04 -0300

    merge

--- dashboard/src/Classes/Axios.js
+++ dashboard/src/Classes/Axios.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-<<<<<<< HEAD
     baseURL:"http://localhost:8000/api",
     timeout:1000000
-=======
-    baseURL:"https://15c9-45-163-236-187.ngrok.io/api",
-    timeout:10000
->>>>>>> 981237923a8de473f514171ec76564339ac1d6f1
 })
 
 
