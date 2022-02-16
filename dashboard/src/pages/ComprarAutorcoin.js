4ad96a83876973ef86629c2d2fb0706dd3b8cf69
Leonardo Vartuli <leovartuli@live.com>
2022-02-16 01:09:34 -0300

    taxas

--- dashboard/src/pages/ComprarAutorcoin.js
+++ dashboard/src/pages/ComprarAutorcoin.js
@@ -21,7 +21,7 @@ function ComprarAutorcoin() {
         Compre AutorCoins para registrar suas obras!
       </p>
       <p className="text-gray-700 dark:text-gray-300">Ao clicar no botão de compra você será redirecionado para a extensão da Metamask.</p> 
-      <p className="text-gray-700 dark:text-gray-300">A quantidade de AutorCoin adquirida é igual a 1000x (um mil vezes) a quantidade de Ethereum transferida.</p> <br/>
+      <p className="text-gray-700 dark:text-gray-300">A quantidade de AutorCoin adquirida é igual a 1000x (um mil vezes) a quantidade de Ethereum transferida, sendo cobrada uma taxa de AUT$ 0,05.</p> <br/>
 <div>
       <Input type="number" value={valor} onChange={e=>setValor(e.target.value)} size="small" className="mt-1 mb-3" placeholder="Quantidade de AutorCoin" />
       </div>
