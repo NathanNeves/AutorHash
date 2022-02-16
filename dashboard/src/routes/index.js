d7f9cc6411675ff4927333fb44b6f1cced7e2102
Leonardo Vartuli <leovartuli@live.com>
2022-02-16 00:43:37 -0300

    editar anuncio

--- dashboard/src/routes/index.js
+++ dashboard/src/routes/index.js
@@ -17,22 +17,14 @@ const ComprarAutorcoin = lazy(() => import('../pages/ComprarAutorcoin'))
 const Obra = lazy(() => import('../pages/ObraInfo'))
 const Anuncio = lazy(() => import('../pages/AnuncioInfo'))
 const CriarAnuncio = lazy(() => import('../pages/CriarAnuncio'))
+const EditarAnuncio = lazy(() => import('../pages/EditarAnuncio'))
 
 
-/**
- * ⚠ These are internal routes!
- * They will be rendered inside the app, using the default `containers/Layout`.
- * If you want to add a route to, let's say, a landing page, you should add
- * it to the `App`'s router, exactly like `Login`, `CreateAccount` and other pages
- * are routed.
- *
- * If you're looking for the links rendered in the SidebarContent, go to
- * `routes/sidebar.js`
- */
+
 const routes = [
   {
-    path: '/dashboard', // the url
-    component: Dashboard, // view rendered
+    path: '/dashboard', 
+    component: Dashboard, 
   },
   {
     path: '/forms',
@@ -93,6 +85,10 @@ const routes = [
   {
     path: '/criaranuncio',
     component: CriarAnuncio,
+  },
+  {
+    path: '/editaranuncio',
+    component: EditarAnuncio,
   },
 ]
 
